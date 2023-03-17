fdb595d8eeda06f3ad9eab1f5b3112f44c71b721
Pavel <kulikovps2004@gmail.com>
2023-03-17 14:02:30 +0300

    Доделана корзина

--- components/ui/Popups/CookiesConsentPopup.tsx
+++ components/ui/Popups/CookiesConsentPopup.tsx
@@ -15,22 +15,18 @@ export const CookiesConsentPopup = () => {
         setConsentGiven(true);
     };
 
-    return (
-        <>
-            {!consentGiven && (
-                <div
-                    className="fixed bottom-0 left-0 right-0 bg-gray-200 text-gray-800 p-4 md:p-6 flex flex-col md:flex-row items-center justify-between">
-                    <p className="text-sm md:text-base md:mr-4">
-                        Мы используем Cookies, чтобы улучшить ваше взаимодействие с нашим сайтом.
-                    </p>
-                    <button
-                        className="bg-white text-gray-800 px-4 py-2 rounded-md mt-4 md:mt-0"
-                        onClick={handleConsent}
-                    >
-                        Я согласен
-                    </button>
-                </div>
-            )}
-        </>
-    );
+    return !consentGiven ? (
+        <div
+            className="fixed bottom-0 left-0 right-0 bg-gray-200 text-gray-800 p-4 md:p-6 flex flex-col md:flex-row items-center justify-between">
+            <p className="text-sm md:text-base md:mr-4">
+                Мы используем Cookies, чтобы улучшить ваше взаимодействие с нашим сайтом.
+            </p>
+            <button
+                className="bg-white text-gray-800 px-4 py-2 rounded-md mt-4 md:mt-0"
+                onClick={handleConsent}
+            >
+                Я согласен
+            </button>
+        </div>
+    ) : null
 };
--- components/ui/Popups/LogoutPopup.tsx
+++ components/ui/Popups/LogoutPopup.tsx
@@ -8,61 +8,57 @@ export const LogoutPopup: FC<IPopup> = ({showModal, setShowModal}) => {
         setShowModal(false)
         replace('/auth/logout')
     }
-    return (
+    return showModal ? (
         <>
-            {showModal ? (
-                <>
-                    <div className="fixed inset-0 z-30 overflow-y-auto">
-                        <div
-                            className="fixed inset-0 w-full h-full bg-black opacity-40"
-                            onClick={() => setShowModal(false)}
-                        ></div>
-                        <div className="flex items-center min-h-screen px-4 py-8">
+            <div className="fixed inset-0 z-30 overflow-y-auto">
+                <div
+                    className="fixed inset-0 w-full h-full bg-black opacity-40"
+                    onClick={() => setShowModal(false)}
+                ></div>
+                <div className="flex items-center min-h-screen px-4 py-8">
+                    <div
+                        className="relative w-full max-w-lg p-4 mx-auto bg-white rounded-md shadow-lg dark:bg-gray-700">
+                        <div className="mt-3">
                             <div
-                                className="relative w-full max-w-lg p-4 mx-auto bg-white rounded-md shadow-lg dark:bg-gray-700">
-                                <div className="mt-3">
-                                    <div
-                                        className="flex items-center justify-center flex-none w-12 h-12 mx-auto bg-red-100 rounded-full">
-                                        <svg
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            className="w-6 h-6 text-red-600"
-                                            viewBox="0 0 20 20"
-                                            fill="currentColor"
-                                        >
-                                            <path
-                                                fillRule="evenodd"
-                                                d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
-                                                clipRule="evenodd"
-                                            />
-                                        </svg>
-                                    </div>
-                                    <div className="mt-2 text-center sm:text-left">
-                                        <h4 className="text-lg font-medium text-gray-800 text-center dark:text-white">
-                                            Вы точно хотите выйти?
-                                        </h4>
-                                        <div className="items-center gap-2 mt-3 sm:flex">
-                                            <button
-                                                className="w-full mt-2 p-2 flex-1 text-white bg-red-600 rounded-md outline-none ring-offset-2 ring-red-600 focus:ring-2"
-                                                onClick={() => logout()}
-                                            >
-                                                Выйти
-                                            </button>
-                                            <button
-                                                className="w-full mt-2 p-2 flex-1 text-gray-800 rounded-md outline-none border ring-offset-2 ring-indigo-600 focus:ring-2 dark:bg-white dark:text-black"
-                                                onClick={() =>
-                                                    setShowModal(false)
-                                                }
-                                            >
-                                                Назад
-                                            </button>
-                                        </div>
-                                    </div>
+                                className="flex items-center justify-center flex-none w-12 h-12 mx-auto bg-red-100 rounded-full">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="w-6 h-6 text-red-600"
+                                    viewBox="0 0 20 20"
+                                    fill="currentColor"
+                                >
+                                    <path
+                                        fillRule="evenodd"
+                                        d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
+                                        clipRule="evenodd"
+                                    />
+                                </svg>
+                            </div>
+                            <div className="mt-2 text-center sm:text-left">
+                                <h4 className="text-lg font-medium text-gray-800 text-center dark:text-white">
+                                    Вы точно хотите выйти?
+                                </h4>
+                                <div className="items-center gap-2 mt-3 sm:flex">
+                                    <button
+                                        className="w-full mt-2 p-2 flex-1 text-white bg-red-600 rounded-md outline-none ring-offset-2 ring-red-600 focus:ring-2"
+                                        onClick={() => logout()}
+                                    >
+                                        Выйти
+                                    </button>
+                                    <button
+                                        className="w-full mt-2 p-2 flex-1 text-gray-800 rounded-md outline-none border ring-offset-2 ring-indigo-600 focus:ring-2 dark:bg-white dark:text-black"
+                                        onClick={() =>
+                                            setShowModal(false)
+                                        }
+                                    >
+                                        Назад
+                                    </button>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </>
-            ) : null}
+                </div>
+            </div>
         </>
-    )
+    ) : null
 }
